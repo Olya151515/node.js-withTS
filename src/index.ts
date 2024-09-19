e28b4d93f7232bacfea4a68c044850ba45099b8a
Olya <olachobanuk@gmail.com>
2024-09-19 09:47:25 +0200

    finished commit Lesson4

--- src/index.ts
+++ src/index.ts
@@ -10,28 +10,6 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/users", userRouter);
 
-// app.delete(
-//   "/users/:id",
-//   async (req: Request, res: Response, next: NextFunction) => {
-//     try {
-//       const hasNumber = /\d/.test(req.params.id);
-//       if (hasNumber) {
-//         const id = Number(req.params.id);
-//         const users = await read();
-//         const userIndex = users.findIndex((user) => user.id === id);
-//         if (userIndex === -1) {
-//           throw new ApiErrors("Not found user", 404);
-//         }
-//         users.splice(userIndex, 1);
-//         await write(users);
-//         return res.sendStatus(204);
-//       }
-//     } catch (e) {
-//       next(e);
-//     }
-//   },
-// );
-
 app.use(
   "*",
   (err: ApiErrors, req: Request, res: Response, next: NextFunction) => {
